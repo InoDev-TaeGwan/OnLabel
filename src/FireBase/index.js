563f25288eccc4d0c95dbff6b0f78b3790a2a0c5
LookerKy <nagaraz07@gmail.com>
2021-10-22 18:28:07 +0900

    add firebase option

---
+++ src/FireBase/index.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFireStore, collection, getDocs } from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCIxS1mzVTL1NUGlIm4_KRs1UVTqwgLEuY",
+  authDomain: "onlabel-cbe22.firebaseapp.com",
+  projectId: "onlabel-cbe22",
+  storageBucket: "onlabel-cbe22.appspot.com",
+  messagingSenderId: "485083663101",
+  appId: "1:485083663101:web:c091f4b15e00c44bf14c94",
+  measurementId: "G-2DK1X6TW1N",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFireStore(app);
+
+db.collection();
---
+++ src/components/Admin/SignInComponent/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignInComponent = () => {
+  return <>Sign in</>;
+};
+
+export default SignInComponent;
