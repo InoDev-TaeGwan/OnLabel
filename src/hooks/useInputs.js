1ec0f62877116fb6c0fcc8344901660cbb39c55f
LookerKy <nagaraz07@gmail.com>
2021-10-25 16:25:57 +0900

    add validator

--- src/hooks/useInputs.js
+++ src/hooks/useInputs.js
@@ -2,16 +2,16 @@ import { useState, useCallback } from 'react';
 
 const useInputs = (initData) => {
     const [form, setForm] = useState(initData);
-
     const onChange = useCallback((event) => {
         const {
             target: { name, value },
         } = event;
+
         setForm((prev) => ({ ...prev, [name]: value }));
     }, []);
 
     const reset = useCallback(() => setForm(initData), [initData]);
-    return [form, onChange, reset]
+    return [form, onChange, reset];
 };
 
-export default useInputs
+export default useInputs;
