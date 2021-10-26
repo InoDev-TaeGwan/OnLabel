c12ad37cc86b2199e5ba737f7e4bf13bffbf4789
KimTaeGwan <development.s@iuproject.me>
2021-10-26 16:47:32 +0900

    Edit profile image

--- src/utils/dumy.js
+++ src/utils/dumy.js
@@ -1,18 +1,18 @@
 /* TEAM */
-import ceo from "assets/images/profile/ceo.jpg";
-import cto from "assets/images/profile/cto.jpg";
-import cso from "assets/images/profile/cso.png";
-import dev1 from "assets/images/profile/dev1.jpeg";
-import dev2 from "assets/images/profile/dev2.jpg";
-import des1 from "assets/images/profile/des1.jpg";
-import des2 from "assets/images/profile/des2.png";
-import jose from "assets/images/profile/JOSE.png";
-import kemo from "assets/images/profile/KEMO.png";
-import itpm from "assets/images/profile/itpm.jpg";
-import leehaegyung from "assets/images/profile/leehaegyung.jpg";
-import seajoonyoung from "assets/images/profile/seajoonyoung.jpg";
-import kimyoojin from "assets/images/profile/kimyoojin.PNG";
-import naduckyoung from "assets/images/profile/naduckyoung.png";
+import ceo from 'assets/images/profile/ceo.jpg';
+import cto from 'assets/images/profile/cto.jpg';
+import cso from 'assets/images/profile/cso.png';
+import dev1 from 'assets/images/profile/dev1.png';
+import dev2 from 'assets/images/profile/dev2.jpg';
+import des1 from 'assets/images/profile/des1.jpg';
+import des2 from 'assets/images/profile/des2.png';
+import jose from 'assets/images/profile/JOSE.png';
+import kemo from 'assets/images/profile/KEMO.png';
+import itpm from 'assets/images/profile/itpm.jpg';
+import leehaegyung from 'assets/images/profile/leehaegyung.jpg';
+import seajoonyoung from 'assets/images/profile/seajoonyoung.jpg';
+import kimyoojin from 'assets/images/profile/kimyoojin.png';
+import naduckyoung from 'assets/images/profile/naduckyoung.png';
 
 /* Adviser */
 import jengDaeWook from 'assets/images/profile/jengDaeWook.png';
@@ -24,239 +24,239 @@ import yoondaegen from 'assets/images/profile/yoondaegen.png';
 // import kimbyungha from 'assets/images/profile/kimbyungha.png';
 // import kimjinhing from 'assets/images/profile/kimjinhing.png';
 
-
 export const partnerData = [
-  {
-    id: 1,
-    name: "김 성 수",
-    position: "CEO",
-    image: ceo,
-    profileHistory: `카이스트 기계공학, 산업 및 시스템 공학 졸업
+    {
+        id: 1,
+        name: '김 성 수',
+        position: 'CEO',
+        image: ceo,
+        profileHistory: `카이스트 기계공학, 산업 및 시스템 공학 졸업
 중소기업진흥공단 청년창업 사관학교 3기 졸업
 2015' 카이스트 기업가정신 총장상 대상
 前 카이스트 사회기술혁신연구소 (KAIST RISTI) 팀장
 前 카이스트 휴머노이드 레인보우로보틱스 팀장
 前 레인보우로보틱스 코스닥 IPO 상장 기여 `,
-  },
-  {
-    id: 2,
-    name: "김 건 수",
-    position: "IT PM",
-    image: itpm,
-    profileHistory: `IT Firm – Samsung SDS
+    },
+    {
+        id: 2,
+        name: '김 건 수',
+        position: 'IT PM',
+        image: itpm,
+        profileHistory: `IT Firm – Samsung SDS
 Intranet developer & DBA Mar 1997 – May 1999
 IT Firm – IBM Korea SWG AVP Team
 Informix SE & Domino SE & AVP service PM Jun 1999 – Sep 2019`,
-  },
+    },
 
-  {
-    id: 3,
-    name: "이 해 경",
-    position: "CSO",
-    image: leehaegyung,
-    profileHistory: `IBM 애자일
+    {
+        id: 3,
+        name: '이 해 경',
+        position: 'CSO',
+        image: leehaegyung,
+        profileHistory: `IBM 애자일
 하나은행, NS, 매트라이프, KB금융 수행`,
-  },
-  {
-    id: 4,
-    name: "윤 대 근",
-    position: "사업고문",
-    image: yoondaegen,
-    profileHistory: `前 IBM KOREA 상무
+    },
+    {
+        id: 4,
+        name: '윤 대 근',
+        position: '사업고문',
+        image: yoondaegen,
+        profileHistory: `前 IBM KOREA 상무
 NH투자증권 마이데이터 구축
 KB국민은행 AI ChatBot 구축
 POSCO 빅데이터 플랫폼 구축`,
-  },
-  {
-    id: 5,
-    name: "서 준 영",
-    position: "DEVELOPER",
-    image: seajoonyoung,
-    profileHistory: `現 한국IBM 부장
+    },
+    {
+        id: 5,
+        name: '서 준 영',
+        position: 'DEVELOPER',
+        image: seajoonyoung,
+        profileHistory: `現 한국IBM 부장
 교보,농협,신한 BPM 컨설턴트 수행
 KB,Metlife 클라우드 아키텍트 수행`,
-  },
-  {
-    id: 6,
-    name: "김 유 진",
-    position: "CSO",
-    image: kimyoojin,
-    profileHistory: `일본 오카야마 대학원 박사 수료
+    },
+    {
+        id: 6,
+        name: '김 유 진',
+        position: 'CSO',
+        image: kimyoojin,
+        profileHistory: `일본 오카야마 대학원 박사 수료
 포스코, 삼성SDS, SKT, LG Uplus 프로젝트 수행
 現 LG CNS 데이터 사이언티스트
 前 한국아이비엠 데이터 사이언티스트`,
-  },
-  {
-    id: 7,
-    name: "김 수 본",
-    position: "DEVELOPER",
-    image: cto,
-    profileHistory: `카이스트 기계공학 학/석사 졸업
+    },
+    {
+        id: 7,
+        name: '김 수 본',
+        position: 'DEVELOPER',
+        image: cto,
+        profileHistory: `카이스트 기계공학 학/석사 졸업
 前 지에프테크놀로지 수석연구원
 수술로봇용 햅틱 피드백 연구개발
 경찰/군사훈련용 명중분석시스템, 수술로봇용
 햅틱 피드백연구개발
 유통관리 및 보안솔루션용 워터 마킹 기술 연구개발`,
-  },
-  {
-    id: 8,
-    name: "이 규 영",
-    position: "DEVELOPER",
-    image: dev1,
-    profileHistory: `블록체인,Wallet,Exchange 등 개발이력 多
+    },
+    {
+        id: 8,
+        name: '이 규 영',
+        position: 'DEVELOPER',
+        image: dev1,
+        profileHistory: `블록체인,Wallet,Exchange 등 개발이력 多
 現 Label Developer
 前 분산처리시스템 연구실 연구원
 前 한이음 프로젝트 참여`,
-  },{
-    id: 9,
-    name: "김 재 웅",
-    position: "DEVELOPER",
-    image: dev2,
-    profileHistory: `서울대학교 컴퓨터공학과
+    },
+    {
+        id: 9,
+        name: '김 재 웅',
+        position: 'DEVELOPER',
+        image: dev2,
+        profileHistory: `서울대학교 컴퓨터공학과
 Python bot 다수 개발
 Unity 3D, C# 게임 개발
 Flutter 앱 개발`,
-  },
+    },
 
-  {
-    id: 10,
-    name: "박 소 희",
-    position: "DESIGNER",
-    image: des1,
-    profileHistory: `한양대학교 디자인과
+    {
+        id: 10,
+        name: '박 소 희',
+        position: 'DESIGNER',
+        image: des1,
+        profileHistory: `한양대학교 디자인과
 前 인디텍스 자라 근무
 前 이랜드 근무
 現 Label Designer`,
-  },
+    },
 
-  {
-    id: 11,
-    name: "성 해 진",
-    position: "DESIGNER",
-    image: des2,
-    profileHistory: `Farne construction(캐나다), Designer
+    {
+        id: 11,
+        name: '성 해 진',
+        position: 'DESIGNER',
+        image: des2,
+        profileHistory: `Farne construction(캐나다), Designer
 KMJ Design Works, Design Team Manager
 DNI Company(베트남, 한국) 디자이너`,
-  },
+    },
 
-  {
-    id: 12,
-    name: "이 성 욱",
-    position: "CSO",
-    image: cso,
-    profileHistory: `블록체인 컨설턴트
+    {
+        id: 12,
+        name: '이 성 욱',
+        position: 'CSO',
+        image: cso,
+        profileHistory: `블록체인 컨설턴트
 Big Stone CEO
 Director of the Btrade Exchange`,
-  },
-  {
-    id: 13,
-    name: "JOSE",
-    position: "ENGLISH MANAGER",
-    image: jose,
-    profileHistory: `성균관대 전자공학석사
+    },
+    {
+        id: 13,
+        name: 'JOSE',
+        position: 'ENGLISH MANAGER',
+        image: jose,
+        profileHistory: `성균관대 전자공학석사
 Korda.us 중남미
 정보통신 기술 컨설팅`,
-  },
+    },
 
-  {
-    id: 14,
-    name: "KEMO",
-    position: "ARABIC MANAGER",
-    image: kemo,
-    profileHistory: `Jubail, Saudi Arabia
+    {
+        id: 14,
+        name: 'KEMO',
+        position: 'ARABIC MANAGER',
+        image: kemo,
+        profileHistory: `Jubail, Saudi Arabia
 성균관대 전자공학석사`,
-  },
-  {
-    id: 15,
-    name: "나 덕 영",
-    position: "CHINA MANAGER",
-    image: naduckyoung,
-    profileHistory: `성균관대 중문학과
+    },
+    {
+        id: 15,
+        name: '나 덕 영',
+        position: 'CHINA MANAGER',
+        image: naduckyoung,
+        profileHistory: `성균관대 중문학과
 중국 SONY전자 및 외주업체 근무
 중국 텐센트엑셀레이터
 ㈜ Label 중국사업부 이사`,
-  },
+    },
 ];
 
 export const adviserData = [
-  {
-    id: 1,
-    name: "정 대 욱",
-    position: "사업고문",
-    image: jengDaeWook,
-    profileHistory: `서강대학교 컴퓨터공학과 석사
+    {
+        id: 1,
+        name: '정 대 욱',
+        position: '사업고문',
+        image: jengDaeWook,
+        profileHistory: `서강대학교 컴퓨터공학과 석사
 前 IBM KOREA 부사장 ( IBM 30년 근속 )
 現 수원과학기술대학교 컴퓨터공학과 교수
 `,
-  },
-  {
-    id: 2,
-    name: "윤 대 근",
-    position: "사업고문",
-    image: yoondaegen,
-    profileHistory: `前 IBM KOREA 상무
+    },
+    {
+        id: 2,
+        name: '윤 대 근',
+        position: '사업고문',
+        image: yoondaegen,
+        profileHistory: `前 IBM KOREA 상무
 NH투자증권 마이데이터 구축
 KB국민은행 AI ChatBot 구축
 POSCO 빅데이터 플랫폼 구축`,
-  },
-  {
-    id: 3,
-    name: "임 경 수",
-    position: "사업고문",
-    image: limgyungsoo,
-    profileHistory: `前 KPMG 금융리스크 컨설턴트
+    },
+    {
+        id: 3,
+        name: '임 경 수',
+        position: '사업고문',
+        image: limgyungsoo,
+        profileHistory: `前 KPMG 금융리스크 컨설턴트
 농협, BC, KB카드 차세대 IT 시스템 개발 PMO
 前 파운더스인베스트먼트 금융 컨설턴트
 現 EY한영회계법인 이사`,
-  },
-  {
-    id: 4,
-    name: "서 광 수",
-    position: "사업고문",
-    image: gwangsoo,
-    profileHistory: `前 KT 네트워크본부 과장
+    },
+    {
+        id: 4,
+        name: '서 광 수',
+        position: '사업고문',
+        image: gwangsoo,
+        profileHistory: `前 KT 네트워크본부 과장
 前 KTF IT인프라운영팀 부장
 前 KTds uCloud 센터장, 상무이사
 現 (주)씨엔아이테크 대표이사`,
-  },
-  {
-    id: 5,
-    name: "WooJoon Cha",
-    position: "사업고문",
-    image: ChaWooJoon,
-    profileHistory: `서울대학교 기계공학과, 항공우주공학과 졸업
+    },
+    {
+        id: 5,
+        name: 'WooJoon Cha',
+        position: '사업고문',
+        image: ChaWooJoon,
+        profileHistory: `서울대학교 기계공학과, 항공우주공학과 졸업
 ELYSIA CSO
 ArtnGuide CTO
 BTbridge lnc. CEO`,
-  },
+    },
 
-  {
-    id: 6,
-    name: "Feng Shiliang",
-    position: "사업고문",
-    image: fengShiliang,
-    profileHistory: `서울대학교 사회학과 졸업
+    {
+        id: 6,
+        name: 'Feng Shiliang',
+        position: '사업고문',
+        image: fengShiliang,
+        profileHistory: `서울대학교 사회학과 졸업
 MODORI 파트너
 thepaper.cn reporter of culture dept.`,
-  },
-//   {
-//     id: 2,
-//     name: "김 병 하",
-//     position: "행정고문",
-//     image: kimbyungha,
-//     profileHistory: `서울특별시 부시장
-// 서울특별시 도시계획 국장
-// 서울특별시 도시안전 실장`,
-//   },
-//   {
-//     id: 5,
-//     name: "김 진 홍",
-//     position: "행정고문",
-//     image: kimjinhing,
-//     profileHistory: `청와대 경제수석실 비서
-// IAEA P5
-// 교육시설공제회 회장
-// 교육부 국장
-// 과학기술부 국장`,
-//   },
-]
+    },
+    //   {
+    //     id: 2,
+    //     name: "김 병 하",
+    //     position: "행정고문",
+    //     image: kimbyungha,
+    //     profileHistory: `서울특별시 부시장
+    // 서울특별시 도시계획 국장
+    // 서울특별시 도시안전 실장`,
+    //   },
+    //   {
+    //     id: 5,
+    //     name: "김 진 홍",
+    //     position: "행정고문",
+    //     image: kimjinhing,
+    //     profileHistory: `청와대 경제수석실 비서
+    // IAEA P5
+    // 교육시설공제회 회장
+    // 교육부 국장
+    // 과학기술부 국장`,
+    //   },
+];
