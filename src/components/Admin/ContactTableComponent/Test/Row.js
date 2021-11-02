fff52ae118b8e88d7f548a238927a7edd307c0d8
LookerKy <nagaraz07@gmail.com>
2021-11-02 18:03:35 +0900

    stea login page design fixed

--- src/components/Admin/ContactTableComponent/Test/Row.js
+++ src/components/Admin/ContactTableComponent/Test/Row.js
@@ -29,98 +29,216 @@ const Row = ({ row }) => {
 
     return (
         <>
-            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-                <TableCell style={{ width: 10 }}>
-                    <IconButton
-                        aria-label="expand row"
-                        size="small"
-                        onClick={() => setOpen(!open)}
+            {open ? (
+                <>
+                    <TableRow
+                        sx={{
+                            '& > *': {
+                                borderBottom: 'unset',
+                                background: '#EEEEEE',
+                            },
+                        }}
                     >
-                        {/*{open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}*/}
-                        {open ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
-                    </IconButton>
-                </TableCell>
-                <TableCell align="center" style={{ width: 28 }}>
-                    {row.index}
-                </TableCell>
-                <TableCell align="center" style={{ width: '18%' }}>
-                    {row.name}
-                </TableCell>
-                <TableCell align="center" style={{ width: '20%' }}>
-                    {row.email}
-                </TableCell>
-                <TableCell align="center" style={{ width: 128 }}>
-                    {row.compareNow}
-                </TableCell>
-                <TableCell
-                    align="center"
-                    className={row.isContact ? `checkContact` : 'noneContact'}
-                    style={{ width: 128 }}
-                >
-                    {row.isContact ? '확인' : '미확인'}
-                </TableCell>
-                <TableCell align="center" style={{ width: 100 }}>
-                    <DeleteIcon
-                        style={{ cursor: 'pointer' }}
-                        onClick={onDelete}
-                    />
-                </TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell
-                    style={{
-                        padding: 0,
-                    }}
-                    colSpan={7}
-                >
-                    <Collapse in={open} timeout="auto" unmountOnExit>
-                        <Box
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                margin: 0,
-                            }}
+                        <TableCell style={{ width: 10 }}>
+                            <IconButton
+                                aria-label="expand row"
+                                size="small"
+                                onClick={() => setOpen(!open)}
+                            >
+                                {/*{open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}*/}
+                                {open ? (
+                                    <ArrowDropUpIcon />
+                                ) : (
+                                    <ArrowDropDownIcon />
+                                )}
+                            </IconButton>
+                        </TableCell>
+                        <TableCell align="center" style={{ width: 28 }}>
+                            {row.index}
+                        </TableCell>
+                        <TableCell align="center" style={{ width: '18%' }}>
+                            {row.name}
+                        </TableCell>
+                        <TableCell align="center" style={{ width: '20%' }}>
+                            {row.email}
+                        </TableCell>
+                        <TableCell align="center" style={{ width: 128 }}>
+                            {row.compareNow}
+                        </TableCell>
+                        <TableCell
+                            align="center"
+                            className={
+                                row.isContact ? `checkContact` : 'noneContact'
+                            }
+                            style={{ width: 128 }}
                         >
-                            <div className="Scroll-wrap">
-                                <div className="Contact-content">
-                                    {row.message
-                                        .split('<br>')
-                                        .map((line, index) => (
-                                            <span key={index}>
-                                                {line}
-                                                <br />
-                                            </span>
-                                        ))}
-                                </div>
-                            </div>
-                            {!row.isContact && (
-                                <div className="Button-wrap">
-                                    <Button
-                                        onClick={onUpdate}
-                                        variant="outlined"
-                                        color={'success'}
-                                        startIcon={
-                                            <CheckIcon color="success" />
-                                        }
-                                    >
-                                        {/*<FontAwesomeIcon icon={faCheckCircle}  />*/}
-                                        답변확인
-                                    </Button>
-                                </div>
-                            )}
-                            {/*<Typography*/}
-                            {/*    variant="h6"*/}
-                            {/*    gutterBottom*/}
-                            {/*    component="span"*/}
-                            {/*>*/}
-                            {/*    History :*/}
-                            {/*</Typography>*/}
-                        </Box>
-                    </Collapse>
-                </TableCell>
-            </TableRow>
+                            {row.isContact ? '확인' : '미확인'}
+                        </TableCell>
+                        <TableCell align="center" style={{ width: 100 }}>
+                            <DeleteIcon
+                                style={{ cursor: 'pointer' }}
+                                onClick={onDelete}
+                            />
+                        </TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell
+                            style={{
+                                padding: 0,
+                            }}
+                            colSpan={7}
+                        >
+                            <Collapse in={open} timeout="auto" unmountOnExit>
+                                <Box
+                                    sx={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        margin: 0,
+                                    }}
+                                >
+                                    <div className="Scroll-wrap">
+                                        <div className="Contact-content">
+                                            {row.message
+                                                .split('<br>')
+                                                .map((line, index) => (
+                                                    <span key={index}>
+                                                        {line}
+                                                        <br />
+                                                    </span>
+                                                ))}
+                                        </div>
+                                    </div>
+                                    {!row.isContact && (
+                                        <div className="Button-wrap">
+                                            <Button
+                                                onClick={onUpdate}
+                                                variant="outlined"
+                                                color={'success'}
+                                                startIcon={
+                                                    <CheckIcon color="success" />
+                                                }
+                                            >
+                                                {/*<FontAwesomeIcon icon={faCheckCircle}  />*/}
+                                                답변확인
+                                            </Button>
+                                        </div>
+                                    )}
+                                    {/*<Typography*/}
+                                    {/*    variant="h6"*/}
+                                    {/*    gutterBottom*/}
+                                    {/*    component="span"*/}
+                                    {/*>*/}
+                                    {/*    History :*/}
+                                    {/*</Typography>*/}
+                                </Box>
+                            </Collapse>
+                        </TableCell>
+                    </TableRow>
+                </>
+            ) : (
+                <>
+                    <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+                        <TableCell style={{ width: 10 }}>
+                            <IconButton
+                                aria-label="expand row"
+                                size="small"
+                                onClick={() => setOpen(!open)}
+                            >
+                                {/*{open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}*/}
+                                {open ? (
+                                    <ArrowDropUpIcon />
+                                ) : (
+                                    <ArrowDropDownIcon />
+                                )}
+                            </IconButton>
+                        </TableCell>
+                        <TableCell align="center" style={{ width: 28 }}>
+                            {row.index}
+                        </TableCell>
+                        <TableCell align="center" style={{ width: '18%' }}>
+                            {row.name}
+                        </TableCell>
+                        <TableCell align="center" style={{ width: '20%' }}>
+                            {row.email}
+                        </TableCell>
+                        <TableCell align="center" style={{ width: 128 }}>
+                            {row.compareNow}
+                        </TableCell>
+                        <TableCell
+                            align="center"
+                            className={
+                                row.isContact ? `checkContact` : 'noneContact'
+                            }
+                            style={{ width: 128 }}
+                        >
+                            {row.isContact ? '확인' : '미확인'}
+                        </TableCell>
+                        <TableCell align="center" style={{ width: 100 }}>
+                            <DeleteIcon
+                                style={{ cursor: 'pointer' }}
+                                onClick={onDelete}
+                            />
+                        </TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell
+                            style={{
+                                padding: 0,
+                            }}
+                            colSpan={7}
+                        >
+                            <Collapse in={open} timeout="auto" unmountOnExit>
+                                <Box
+                                    sx={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        margin: 0,
+                                    }}
+                                >
+                                    <div className="Scroll-wrap">
+                                        <div className="Contact-content">
+                                            {row.message
+                                                .split('<br>')
+                                                .map((line, index) => (
+                                                    <span key={index}>
+                                                        {line}
+                                                        <br />
+                                                    </span>
+                                                ))}
+                                        </div>
+                                    </div>
+                                    {!row.isContact && (
+                                        <div className="Button-wrap">
+                                            <Button
+                                                onClick={onUpdate}
+                                                variant="outlined"
+                                                color={'success'}
+                                                startIcon={
+                                                    <CheckIcon color="success" />
+                                                }
+                                            >
+                                                {/*<FontAwesomeIcon icon={faCheckCircle}  />*/}
+                                                답변확인
+                                            </Button>
+                                        </div>
+                                    )}
+                                    {/*<Typography*/}
+                                    {/*    variant="h6"*/}
+                                    {/*    gutterBottom*/}
+                                    {/*    component="span"*/}
+                                    {/*>*/}
+                                    {/*    History :*/}
+                                    {/*</Typography>*/}
+                                </Box>
+                            </Collapse>
+                        </TableCell>
+                    </TableRow>
+                </>
+            )}
         </>
     );
 };
--- src/components/Admin/SignInComponent/index.js
+++ src/components/Admin/SignInComponent/index.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
 import { authService } from '../../../FireBase';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const SignInComponent = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
+    const theme = createTheme();
 
     const onChange = (event) => {
         const {
@@ -18,9 +31,8 @@ const SignInComponent = () => {
         }
     };
 
-    const onClick = async (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
-
         try {
             await authService.signInWithEmailAndPassword(email, password);
         } catch (error) {
@@ -28,41 +40,111 @@ const SignInComponent = () => {
         }
     };
     return (
-        <div className="signInContainer">
-            <div className="formContainer">
-                <div className="signIn-header">
-                    <span className="signIn-title">Label Admin</span>
-                </div>
-                <div className="inputContainer">
-                    <span className="label">Email</span>
-                    <input
-                        name="email"
-                        type="text"
-                        value={email}
-                        onChange={onChange}
-                        placeholder="example@example.com"
-                    />
-                </div>
-                <div className="inputContainer">
-                    <span className="label">Password</span>
-                    <input
-                        name="password"
-                        type="password"
-                        value={password}
-                        onChange={onChange}
-                        placeholder="password"
-                    />
-                </div>
-                {error ? (
-                    <span className="errorMsg">
-                        * 이메일 혹은 비밀번호를 확인해주세요
-                    </span>
-                ) : null}
-                <div className="buttonContainer">
-                    <button onClick={onClick}>Sign In</button>
-                </div>
-            </div>
-        </div>
+        <ThemeProvider theme={theme}>
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <CssBaseline />
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={7}
+                    sx={{
+                        backgroundImage:
+                            'url(https://storage.googleapis.com/onlabel-cbe22.appspot.com/logo/Logo.png)',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundColor: (t) =>
+                            t.palette.mode === 'dark'
+                                ? t.palette.grey[50]
+                                : t.palette.grey[900],
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'center',
+                    }}
+                />
+                <Grid
+                    item
+                    xs={12}
+                    sm={8}
+                    md={5}
+                    component={Paper}
+                    elevation={6}
+                    square
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Box
+                        sx={{
+                            my: 8,
+                            mx: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            marginTop: 0,
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'greenyellow' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign in
+                        </Typography>
+                        <Box
+                            component="form"
+                            noValidate
+                            onSubmit={onSubmit}
+                            sx={{ mt: 1 }}
+                        >
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                onChange={onChange}
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                onChange={onChange}
+                            />
+                            {error ? (
+                                <span className="errorMsg">
+                                    * 이메일 혹은 비밀번호를 확인해주세요
+                                </span>
+                            ) : null}
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        value="remember"
+                                        color="primary"
+                                    />
+                                }
+                                label="Remember me"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign In
+                            </Button>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </ThemeProvider>
     );
 };
 
